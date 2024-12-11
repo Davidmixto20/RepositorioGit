f0728addaa4297f67e6c08697c8c3ba5d78c0e3d
David Reyes <davidaniel0508@gmaill.com>
2024-12-11 10:24:21 -0400

    bool yandell true

--- RepositorioGit/Form1.cs
+++ RepositorioGit/Form1.cs
@@ -12,9 +12,15 @@ namespace RepositorioGit
 {
     public partial class Form1 : Form
     {
+        bool yandell = true;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
