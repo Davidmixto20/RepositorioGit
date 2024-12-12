cd1d1f6ffb98214fbe8d91dbd610bb1570387e8b
David Reyes <davidaniel0508@gmaill.com>
2024-12-12 09:38:04 -0400

    muchos cambios en el codigo

--- RepositorioGit/Form1.cs
+++ RepositorioGit/Form1.cs
@@ -1,16 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 
+//Yandell
 namespace RepositorioGit
 {
     public partial class Form1 : Form
     {
-        // Cadena de conexión a SQL Server
         string connectionString = "Server=.;Database=AgendaDB;Trusted_Connection=True;";
 
-        // Variable para almacenar el ID del contacto seleccionado para editar
+
+        //Variable para almacenar el ID del contacto seleccionado para editar
         private int contactoID = -1;
 
         public Form1()
@@ -19,7 +26,7 @@ namespace RepositorioGit
             CargarContactos();
         }
 
-        // Método para agregar o actualizar un contacto
+        //Metodo para agregar o actualizar un contacto
         private void btnAgregar_Click(object sender, EventArgs e)
         {
             if (contactoID == -1)
@@ -32,13 +39,13 @@ namespace RepositorioGit
             }
         }
 
-        // Método para agregar un nuevo contacto
+        //Metodo para agregar nuevo contacto
         private void AgregarContacto()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                string query = "INSERT INTO Contactos (Nombre, Apellido, Telefono, Correo, Direccion) " +
-                               "VALUES (@Nombre, @Apellido, @Telefono, @Correo, @Direccion)";
+                string query = "Insert into Contactos (Nombre, Apellido, Telefono, Correo, Direccion) " +
+                               "Values (@Nombre, @Apellido, @Telefono, @Correo, @Direccion)";
                 SqlCommand cmd = new SqlCommand(query, connection);
                 cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
                 cmd.Parameters.AddWithValue("@Apellido", txtApellido.Text);
@@ -50,19 +57,20 @@ namespace RepositorioGit
                 cmd.ExecuteNonQuery();
                 connection.Close();
 
-                MessageBox.Show("Contacto agregado exitosamente.");
+                MessageBox.Show("Contacto agregado exitosamente");
                 CargarContactos();
-                LimpiarCampos();
+                //LimpiarCampos();
+
             }
         }
 
-        // Método para actualizar un contacto existente
+        //Metodo para actualizar un contacto existente
         private void ActualizarContacto()
         {
             using (SqlConnection connection = new SqlConnection(connectionString))
             {
                 string query = "UPDATE Contactos SET Nombre = @Nombre, Apellido = @Apellido, Telefono = @Telefono, " +
-                               "Correo = @Correo, Direccion = @Direccion WHERE ID = @ID";
+                               "Correo = @Correo, Direccion = @Direccion Where ID = @ID";
                 SqlCommand cmd = new SqlCommand(query, connection);
                 cmd.Parameters.AddWithValue("@ID", contactoID);
                 cmd.Parameters.AddWithValue("@Nombre", txtNombre.Text);
@@ -75,13 +83,14 @@ namespace RepositorioGit
                 cmd.ExecuteNonQuery();
                 connection.Close();
 
-                MessageBox.Show("Contacto actualizado correctamente.");
+                MessageBox.Show("Contacto actualizado exitosamente");
                 CargarContactos();
-                LimpiarCampos();
+                //LimpiarCampos();
+
             }
         }
 
-        // Evento para cargar datos del contacto seleccionado en los TextBox y cambiar el botón a "Actualizar"
+        //Evento para Cargar datos seleccionados y cambiar el boton agregar a actualizar
         private void dgvContactos_CellClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex >= 0)
@@ -95,6 +104,22 @@ namespace RepositorioGit
                 txtDireccion.Text = row.Cells["Direccion"].Value.ToString();
 
                 btnAgregar.Text = "Actualizar";
+            }
+        }
+
+
+        // David
+
+        // Método para cargar los contactos en el DataGridView
+        private void CargarContactos()
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Contactos";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvContactos.DataSource = dt;
             }
         }
 
@@ -117,7 +142,6 @@ namespace RepositorioGit
 
                     MessageBox.Show("Contacto eliminado exitosamente.");
                     CargarContactos();
-                    LimpiarCampos();
                 }
             }
             else
@@ -126,45 +150,17 @@ namespace RepositorioGit
             }
         }
 
-        // Método para buscar contactos por nombre o apellido
-        private void btnBuscar_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "SELECT * FROM Contactos WHERE Nombre LIKE @Nombre OR Apellido LIKE @Apellido";
-                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
-                adapter.SelectCommand.Parameters.AddWithValue("@Nombre", "%" + txtNombre.Text + "%");
-                adapter.SelectCommand.Parameters.AddWithValue("@Apellido", "%" + txtApellido.Text + "%");
 
-                DataTable dt = new DataTable();
-                adapter.Fill(dt);
-                dgvContactos.DataSource = dt;
-            }
-        }
-
-        // Método para cargar los contactos en el DataGridView
-        private void CargarContactos()
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                string query = "SELECT * FROM Contactos";
-                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
-                DataTable dt = new DataTable();
-                adapter.Fill(dt);
-                dgvContactos.DataSource = dt;
-            }
-        }
-
-        // Método para limpiar los campos de texto
-        private void LimpiarCampos()
-        {
-            txtNombre.Clear();
-            txtApellido.Clear();
-            txtTelefono.Clear();
-            txtCorreo.Clear();
-            txtDireccion.Clear();
-            contactoID = -1;
-            btnAgregar.Text = "Agregar";
         }
     }
 }
+//Aaron
+
+// Método para buscar contactos por nombre o apellido (pon tu codigo abajo de esto)
+
+
+
+//Starling
+//Método para limpiar los campos de texto (pon tu codigo abajo de esto)
