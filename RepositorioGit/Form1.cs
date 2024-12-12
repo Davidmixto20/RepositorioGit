f7d4d069b24c0eaf5e70dc6a56be816a80aeb823
David Reyes <davidaniel0508@gmaill.com>
2024-12-12 09:25:45 -0400

    eliminar y buscar

--- RepositorioGit/Form1.cs
+++ RepositorioGit/Form1.cs
@@ -81,5 +81,66 @@ namespace RepositorioGit
             }
         }
 
+        // Evento para cargar datos del contacto seleccionado en los TextBox y cambiar el botón a "Actualizar"
+        private void dgvContactos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow row = dgvContactos.Rows[e.RowIndex];
+                contactoID = Convert.ToInt32(row.Cells["ID"].Value);
+                txtNombre.Text = row.Cells["Nombre"].Value.ToString();
+                txtApellido.Text = row.Cells["Apellido"].Value.ToString();
+                txtTelefono.Text = row.Cells["Telefono"].Value.ToString();
+                txtCorreo.Text = row.Cells["Correo"].Value.ToString();
+                txtDireccion.Text = row.Cells["Direccion"].Value.ToString();
+
+                btnAgregar.Text = "Actualizar";
+            }
+        }
+
+        // Método para eliminar un contacto
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            if (dgvContactos.SelectedRows.Count > 0)
+            {
+                int id = Convert.ToInt32(dgvContactos.SelectedRows[0].Cells["ID"].Value);
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string query = "DELETE FROM Contactos WHERE ID = @ID";
+                    SqlCommand cmd = new SqlCommand(query, connection);
+                    cmd.Parameters.AddWithValue("@ID", id);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+
+                    MessageBox.Show("Contacto eliminado exitosamente.");
+                    CargarContactos();
+                    LimpiarCampos();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Seleccione un contacto para eliminar.");
+            }
+        }
+
+        // Método para buscar contactos por nombre o apellido
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM Contactos WHERE Nombre LIKE @Nombre OR Apellido LIKE @Apellido";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
+                adapter.SelectCommand.Parameters.AddWithValue("@Nombre", "%" + txtNombre.Text + "%");
+                adapter.SelectCommand.Parameters.AddWithValue("@Apellido", "%" + txtApellido.Text + "%");
+
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+                dgvContactos.DataSource = dt;
+            }
+        }
+
     }
 }
