32db0849073505ccc77c8da209cacb806d231033
YandellCM <151237648+YandellCM@users.noreply.github.com>
2024-12-12 20:35:09 -0500

    Update Form1.Designer.cs

--- RepositorioGit/Form1.Designer.cs
+++ RepositorioGit/Form1.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace RepositorioGit
+namespace RepositorioGit
 {
     partial class Form1
     {
-        /// <summary>
-        /// Variable del diseñador necesaria.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Limpiar los recursos que se estén usando.
-        /// </summary>
-        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,14 +13,9 @@
             base.Dispose(disposing);
         }
 
-        #region Código generado por el Diseñador de Windows Forms
-
-        /// <summary>
-        /// Método necesario para admitir el Diseñador. No se puede modificar
-        /// el contenido de este método con el editor de código.
-        /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -38,189 +26,191 @@
             this.txtTelefono = new System.Windows.Forms.TextBox();
             this.txtCorreo = new System.Windows.Forms.TextBox();
             this.txtDireccion = new System.Windows.Forms.TextBox();
-
             this.btnAgregar = new System.Windows.Forms.Button();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.btnEditar = new System.Windows.Forms.Button();
             this.btnBuscar = new System.Windows.Forms.Button();
- Yandell
             this.dgvContactos = new System.Windows.Forms.DataGridView();
-
             ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
- Yandell
-            this.label1.Location = new System.Drawing.Point(23, 84);
+            this.label1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(130, 77);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 13);
-
+            this.label1.Size = new System.Drawing.Size(62, 16);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nombre";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-Yandell
-            this.label2.Location = new System.Drawing.Point(23, 109);
+            this.label2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(130, 139);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-
+            this.label2.Size = new System.Drawing.Size(65, 16);
             this.label2.TabIndex = 1;
             this.label2.Text = "Apellido";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
- Yandell
-            this.label3.Location = new System.Drawing.Point(23, 139);
+            this.label3.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(130, 193);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(49, 13);
-
+            this.label3.Size = new System.Drawing.Size(69, 16);
             this.label3.TabIndex = 2;
             this.label3.Text = "Telefono";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
- Yandell
-            this.label4.Location = new System.Drawing.Point(23, 170);
+            this.label4.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(130, 255);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 13);
-
+            this.label4.Size = new System.Drawing.Size(54, 16);
             this.label4.TabIndex = 3;
             this.label4.Text = "Correo";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
- Yandell
-            this.label5.Location = new System.Drawing.Point(23, 201);
+            this.label5.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(130, 304);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(52, 13);
-
+            this.label5.Size = new System.Drawing.Size(73, 16);
             this.label5.TabIndex = 4;
             this.label5.Text = "Direccion";
             // 
             // txtNombre
             // 
- Yandell
-            this.txtNombre.Location = new System.Drawing.Point(82, 77);
+            this.txtNombre.Location = new System.Drawing.Point(29, 99);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(290, 20);
-
             this.txtNombre.TabIndex = 5;
             // 
             // txtApellido
             // 
- Yandell
-            this.txtApellido.Location = new System.Drawing.Point(82, 106);
+            this.txtApellido.Location = new System.Drawing.Point(29, 158);
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(290, 20);
-
             this.txtApellido.TabIndex = 6;
             // 
             // txtTelefono
             // 
- Yandell
-            this.txtTelefono.Location = new System.Drawing.Point(82, 132);
+            this.txtTelefono.Location = new System.Drawing.Point(29, 212);
             this.txtTelefono.Name = "txtTelefono";
             this.txtTelefono.Size = new System.Drawing.Size(290, 20);
-
             this.txtTelefono.TabIndex = 7;
             // 
             // txtCorreo
             // 
- Yandell
-            this.txtCorreo.Location = new System.Drawing.Point(82, 163);
+            this.txtCorreo.Location = new System.Drawing.Point(29, 274);
             this.txtCorreo.Name = "txtCorreo";
             this.txtCorreo.Size = new System.Drawing.Size(290, 20);
-
             this.txtCorreo.TabIndex = 8;
             // 
             // txtDireccion
             // 
- Yandell
-            this.txtDireccion.Location = new System.Drawing.Point(82, 198);
+            this.txtDireccion.Location = new System.Drawing.Point(29, 335);
             this.txtDireccion.Name = "txtDireccion";
             this.txtDireccion.Size = new System.Drawing.Size(290, 20);
             this.txtDireccion.TabIndex = 9;
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(12, 299);
+            this.btnAgregar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnAgregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnAgregar.Location = new System.Drawing.Point(29, 397);
             this.btnAgregar.Name = "btnAgregar";
-            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
-            this.btnAgregar.TabIndex = 4;
+            this.btnAgregar.Size = new System.Drawing.Size(101, 32);
+            this.btnAgregar.TabIndex = 10;
             this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = false;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // btnEliminar
             // 
-            this.btnEliminar.Location = new System.Drawing.Point(121, 299);
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnEliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnEliminar.Location = new System.Drawing.Point(136, 397);
             this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
-            this.btnEliminar.TabIndex = 3;
+            this.btnEliminar.Size = new System.Drawing.Size(75, 32);
+            this.btnEliminar.TabIndex = 11;
             this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
             this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // btnEditar
             // 
-            this.btnEditar.Location = new System.Drawing.Point(216, 299);
+            this.btnEditar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnEditar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnEditar.Location = new System.Drawing.Point(217, 397);
             this.btnEditar.Name = "btnEditar";
-            this.btnEditar.Size = new System.Drawing.Size(75, 23);
-            this.btnEditar.TabIndex = 2;
+            this.btnEditar.Size = new System.Drawing.Size(75, 32);
+            this.btnEditar.TabIndex = 12;
             this.btnEditar.Text = "Editar";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
             // 
             // btnBuscar
             // 
-            this.btnBuscar.Location = new System.Drawing.Point(297, 299);
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btnBuscar.Location = new System.Drawing.Point(298, 397);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
-            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Size = new System.Drawing.Size(75, 32);
+            this.btnBuscar.TabIndex = 13;
             this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
             // 
             // dgvContactos
             // 
-            this.dgvContactos.Location = new System.Drawing.Point(414, 77);
+            this.dgvContactos.Location = new System.Drawing.Point(399, 77);
             this.dgvContactos.Name = "dgvContactos";
-            this.dgvContactos.Size = new System.Drawing.Size(374, 197);
-            this.dgvContactos.TabIndex = 0;
+            this.dgvContactos.Size = new System.Drawing.Size(611, 352);
+            this.dgvContactos.TabIndex = 14;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dgvContactos);
-
-            this.Controls.Add(this.btnBuscar);
-            this.Controls.Add(this.btnEditar);
-            this.Controls.Add(this.btnEliminar);
-            this.Controls.Add(this.btnAgregar);
-
-            this.Controls.Add(this.txtDireccion);
-            this.Controls.Add(this.txtCorreo);
-            this.Controls.Add(this.txtTelefono);
-            this.Controls.Add(this.txtApellido);
-            this.Controls.Add(this.txtNombre);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1050, 487);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgvContactos);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dgvContactos)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
-
-        #endregion
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -232,14 +222,10 @@ Yandell
         private System.Windows.Forms.TextBox txtTelefono;
         private System.Windows.Forms.TextBox txtCorreo;
         private System.Windows.Forms.TextBox txtDireccion;
-
         private System.Windows.Forms.Button btnAgregar;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Button btnEditar;
         private System.Windows.Forms.Button btnBuscar;
- Yandell
         private System.Windows.Forms.DataGridView dgvContactos;
-
     }
 }
-
